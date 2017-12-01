e2bfcbab71999a52e4cac38ea0b8abc5362817fa
NicolaiCM901 <34155768+NicolaiCM901@users.noreply.github.com>
2017-12-01 12:46:23 +0100

    Menu creazione, private mkBook modificato in public

--- src/library/CreateMenu.java
+++ src/library/CreateMenu.java
@@ -1,8 +1,43 @@
 package library;
 
+import java.util.Map;
+
 public class CreateMenu {
 
-    public static void menu(final Storage storage) {
+    static String readln() {
+        return new java.util.Scanner(System.in).nextLine();
+    }
+    
+    public static void menu(final Storage storage) {   
+        String isbn, title, author, genre, year, risp;
+        Map<String, String> book;
+        System.out.println("Would you like insert a new book? (yes/no)");
+        risp = readln();
+
+        while (risp.equals("yes")){    
+
+        System.out.println("Insert isbn code");
+        isbn = readln();
+        System.out.println("Insert the title");
+        title = readln();
+        System.out.println("Insert the author");
+        author = readln();
+        System.out.println("Insert the genre");
+        genre = readln();
+        System.out.println("Insert the year");
+        year = readln();
+
+        book = DummyStorage.mkBook(isbn, title, author, genre, year);
+        
+        if (storage.save(book)){
+            System.out.println("Book entered");
+        }
+        
+        System.out.println("Would you like insert a another book? (yes/no)");
+        risp = readln();
+        }
+
+
         // write book information prompt
         // read user input
         // magic
